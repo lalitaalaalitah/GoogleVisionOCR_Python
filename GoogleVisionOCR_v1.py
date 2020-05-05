96342d2f576850e84395c50dd155971e1670d2b2
lalitaalaalitah <lalitaalaalitah@l.local>
2020-05-05 23:52:26 +0530

    remove break since it was out loop after removal of while loop

--- GoogleVisionOCR_v1.py
+++ GoogleVisionOCR_v1.py
@@ -86,7 +86,7 @@ if __name__ == "__main__":
                                 )
 
                             except Exception as x:
-                                break
+                                print(x)
 
                         elif path_of_item.endswith(".txt"):
                             os.rename(
